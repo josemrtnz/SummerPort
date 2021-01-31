d81847b0bcb106352b0fbf057c3ffaf1ef22fece
Jose Martinez <49460561+josemrtnz@users.noreply.github.com>
2021-01-31 15:55:12 -0800

    Documentation

--- SummerPort/include/robot-config.h
+++ SummerPort/include/robot-config.h
@@ -3,18 +3,60 @@
 #include "v5_vcs.h"
 using namespace vex;
 
+/// Class for robot configuration.
+///
+/// A class for storing motor, sensor, brain, controller objects.
+/// This class also holds most constants used throught the program.
+/// Classes like \ref odometry, autonomousControl, and userControl will use
+/// this class to access sensor data and motors.
 class robotChasis{
   public:
+    /// Gets pi value.
+    ///
+    /// PI value is derived from the pi approximation 355/113.
+    /// @return pi value as a double
     double getPI();
-    float getsL();
-    float getsR();
-    float getsS();
-    float get_frbl();
-    float get_flbr();
-    double getWheelCir();
 
+    /// Gets left tracking wheel position.
+    ///
+    /// Current left tracking wheel position in degree units will be aqquired using this method.
+    /// @return value of sL as a float.
+    float getsL();
+
+    /// Gets right tracking wheel position.
+    ///
+    /// Current right tracking wheel position in degree units will be aqquired using this method.
+    /// @return value of sR as a float.
+    float getsR();
+
+    /// Gets back tracking wheel position.
+    ///
+    /// Current back tracking wheel position in degree units will be aqquired using this method.
+    /// @return value of sS as a float.
+    float getsS();
+
+    /// Gets frbl value.
+    ///
+    /// Front right and back left wheel constant of 135 degrees.
+    /// @return value frbl as a float
+    float get_frbl();
+
+    /// Gets flbr value.
+    ///
+    /// Front left and back right wheel constant of 45 degrees.
+    /// @return value flbr as a float
+    float get_flbr();
+
+    /// Gets wheel circumference value of the tracking wheels.
+    ///
+    /// Wheel circumference of the right, left, back tracking wheels.
+    /// @return value wheelCir as a double
+    double getWheelCir();
+    
     brain Brain;
+
     competition Comp;
+
     controller Controller1;
 
     motor frontRight = motor(PORT16, ratio18_1, false);
@@ -34,8 +76,27 @@ class robotChasis{
 
     inertial gyroM = inertial(PORT20);
 
+    /// Sets the brake type for the drive motors.
+    ///
+    /// Will set each drive motor to the brake type provided in the parameter field.
+    /// @param B the type of break you want your to set your motors to.
+    /// \see <a href="https://api.vexcode.cloud/v5/html/namespacevex.html#a2090f7d57d63c5cf693f269bc73568f1">vex::brakeType</a>
     void set_drive_break_type(brakeType B);
+
+    /// Stops all drive motors.
+    ///
+    /// Will brake all drive motors according their current brakeType.
+    /// \see <a href="https://api.vexcode.cloud/v5/html/namespacevex.html#a2090f7d57d63c5cf693f269bc73568f1">vex::brakeType</a>
     void stopMotors();
+
+    /// Constructor for the class robotChasis.
+    ///
+    /// This constructor will create an instance of the class robotChasis.
+    /// It will set values to wheelDiameter, sL, sR, and sS.
+    /// @param wD Wheel diameter in inches.
+    /// @param tcL Left Tracking Wheel distance to tracking center. 
+    /// @param tcR Right Tracking Wheel distance to tracking center. 
+    /// @param tcB Back Tracking Wheel distance to tracking center. 
     robotChasis(float wD, float tcL, float tcR, float tcB);
 
   private:
