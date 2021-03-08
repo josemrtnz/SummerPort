d74c077e948ef052ecb27e92bd2219684f5e6b0e
Jose Martinez <49460561+josemrtnz@users.noreply.github.com>
2021-03-07 20:11:14 -0800

    tuning

--- SummerPort/src/main.cpp
+++ SummerPort/src/main.cpp
@@ -9,7 +9,7 @@
 #include "vex.h"
 
 using namespace vex;
-robotChasis simp = robotChasis(2.9, 2.2, 2.2, 6.6);
+robotChasis simp = robotChasis(3.5, 2.3, 2.3, 7);
 odometry tracker = odometry(&simp, 0, 0, 0);
 autonomousControl autoChasis = autonomousControl(&simp, &tracker);
 
@@ -36,7 +36,7 @@ void opControl(){
   
   autoChasis.setPIDConstants(2000, 10, 6000, 5000, 
                              2000, 10, 6000, 5000,
-                             1000, 0, 3500, 0);
+                             800, 0, 3000, 0);
                              
   autoChasis.updateTargetPos(0, 0, 90);
   autoChasis.waitUntilSettled();
